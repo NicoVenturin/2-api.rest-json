fdf5d00477a71d6f28d85eab36832e403c2db4c8
NicoVenturin <145157147+NicoVenturin@users.noreply.github.com>
2024-09-25 16:17:53 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -27,39 +27,39 @@ const escribirDatos = (datos) => {
 
 function reIndex(datos){
     let indice = 1;
-    datos.productos.map((p)=>{
+    datos.productos.map((p)=>{ //map loopea todos los objetos
         p.id = indice;
         indice++;
     });
 }
 app.get('/productos',(req,res)=>{
-    const datos = leerDatos();
-    res.json(datos.productos)
+    const datos = leerDatos(); // returnea todo el json
+    res.json(datos.productos) 
     //res.send('listado de productos');
 });
 
 app.post('/productos',(req,res)=>{
     const datos = leerDatos();    
-    const nuevoProducto={ id:datos.productos.length+1,
+    const nuevoProducto={ id:datos.productos.length+1, //asigna id y copia el body insertado
         ...req.body
     }
-    datos.productos.push(nuevoProducto);
+    datos.productos.push(nuevoProducto); 
     escribirDatos(datos);
     res.json({mensaje:'nuevo producto agregado',
-        producto:nuevoProducto
+        producto:nuevoProducto //devuelve el nuevo objeto
     })
     //res.send('agregar producto');
 });
 
 app.get('/productos/:id',(req,res)=>{
     const datos = leerDatos();    
-    const prodEncontrado = datos.productos.find(p=>p.id == req.params.id);
+    const prodEncontrado = datos.productos.find(p=>p.id == req.params.id); //toma p de parametro y busca un objeto de igual id
     if(!prodEncontrado){
         return res.status(404).json({mensaje:'producto no encontrado'});
     }
     else{
         res.json({mensaje:'producto encontrado',
-            producto:prodEncontrado
+            producto:prodEncontrado //devuelve el objeto encontrado
         });
     }
     
@@ -74,7 +74,7 @@ app.put('/productos/:id',(req,res)=>{
     if(!prodEncontrado){
         return res.status(404).json({mensaje:'producto no encontrado'});
     }
-    datos.productos = datos.productos.map(p=>p.id==req.params.id ? {...p,...nuevosDatos}:p);
+    datos.productos = datos.productos.map(p=>p.id==req.params.id ? {...p,...nuevosDatos}:p); //actualiza el objeto con los datos insertados
     res.json({mensaje:"datos actualizados"});
 });
 
@@ -86,7 +86,7 @@ app.delete('/productos/:id',(req,res)=>{
         return res.status(404),res.json("no se encuentra el producto");
     }
     //filter crea un reemplazo del array sin el borrado
-    datos.productos = datos.productos.filter((p)=>p.id != req.params.id);
+    datos.productos = datos.productos.filter((p)=>p.id != req.params.id); //encuentra el producto con el mismo id, lo filtra del array y reescribe los id para evitar conflictos
     reIndex(datos);
     escribirDatos(datos);
     console.log(datos.productos);
